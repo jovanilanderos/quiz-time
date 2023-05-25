590d60e38d1dc42ea830296ad56a76cc709fa1ac
jovani <jovanilanderos@gmail.com>
2023-05-24 22:20:36 -0500

    stored all questions in a variable 'questions' where the string question is the question itself, choices as an array of strings with the possible answers, 'correctAnswer is the integer representing the index of the right answer in the choices array.

--- script.js
+++ script.js
@@ -13,3 +13,33 @@ var scoreElement = document.getElementById("score");
 var initialsForm = document.getElementById("initials-form");
 var initialsInput = document.getElementById("initials");
 
+var questions = [
+    {
+      question: "Question 1: Which keyword is used to declare variables in JavaScript?",
+      choices: ["var", "let", "const", "int"],
+      correctAnswer: 0
+    },
+    {
+      question: "Question 2: Which method is used to add an element at the end of an array?",
+      choices: ["push()", "pop()", "shift()", "unshift()"],
+      correctAnswer: 0
+    },
+    {
+      question: "Question 3: What is the output of the following code?\n\nconsole.log(typeof null);",
+      choices: ["object", "null", "undefined", "string"],
+      correctAnswer: 0
+    },
+    {
+      question: "Question 4: Which operator is used to concatenate strings in JavaScript?",
+      choices: ["+", "-", "*", "/"],
+      correctAnswer: 0
+    },
+    {
+      question: "Question 5: What does the '===' operator do in JavaScript?",
+      choices: ["Checks for equality in value and type", "Assigns a value to a variable", "Checks for inequality in value and type", "Compares two values for less than or equal to"],
+      correctAnswer: 0
+    }
+  ];
+  
+
+  
