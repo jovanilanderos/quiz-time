1b76bd49f5431f1c8f1e8e81cc56a97dd9fe2ee1
jovani <jovanilanderos@gmail.com>
2023-05-24 21:41:10 -0500

    set classes and ids to html elements, made variables on JS, used getelementbyid to start access html elements

---
+++ script.js
@@ -0,0 +1,15 @@
+var currentQuestionIndex = 0;
+var time = 60;
+var timerInterval;
+var score = 0;
+
+var startButton = document.getElementById("start-btn");
+var questionContainer = document.getElementById("question-container");
+var questionElement = document.getElementById("question");
+var choicesElement = document.getElementById("choices");
+var timerElement = document.getElementById("timer");
+var endContainer = document.getElementById("end-container");
+var scoreElement = document.getElementById("score");
+var initialsForm = document.getElementById("initials-form");
+var initialsInput = document.getElementById("initials");
+
